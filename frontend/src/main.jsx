0263cb0aac677df0b95b118e171fd54a4cf3b36a
Josh Murchie <joshua@murchie.me>
2026-04-10 12:52:34 -0500

    Frontend Performance Optimizations

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import "@fontsource/inter/300.css"
+import "@fontsource/inter/400.css"
+import "@fontsource/inter/600.css"
+import "@fontsource/inter/800.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
